a6e92a33eb0e20a95f1908e8d1c84a4be9cf71fe
Elena Manneh <elenamanneh@gmail.com>
2024-04-09 18:47:51 -0400

    updated description

--- game.asm
+++ game.asm
@@ -10,7 +10,7 @@
 # - Unit width in pixels: 8
 # - Unit height in pixels: 8
 # - Display width in pixels: 512
-# - Display height in pixels: 512 (update this as needed)
+# - Display height in pixels: 512
 # - Base Address for Display: 0x10008000 ($gp)
 #
 # Which milestoneshave been reached in this submission?
